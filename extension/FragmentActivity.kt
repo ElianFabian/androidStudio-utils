15f51036c1bffe7ac0adfe83278e1fef5c0bb2b8
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-24 10:51:21 +0200

    Divide goToFragment() move to FragmentActivity.kt

---
+++ extension/FragmentActivity.kt
@@ -0,0 +1,15 @@
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+
+fun FragmentActivity.goToFragment(fragment: Fragment, args: Bundle? = null)
+{
+    fragment.arguments = args
+
+    supportFragmentManager.beginTransaction().apply()
+    {
+        replace(R.id.nav_host_fragment, fragment)
+        commit()
+    }
+}

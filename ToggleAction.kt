8779d072f9aa1ea67e08a4716e3688f20093370a
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-30 14:40:04 +0200

    Create ToggleAction.kt

---
+++ ToggleAction.kt
@@ -0,0 +1,12 @@
+class ToggleAction(var onOn: () -> Unit = {}, var onOff: () -> Unit = {})
+{
+    private var isOn = true
+
+    fun toggle()
+    {
+        if (isOn) onOn()
+        else onOff()
+
+        isOn = !isOn
+    }
+}

b87c204550d1d96311506d20134b8000ce9e424c
Elián Fabián <86477169+ElianFabian@users.noreply.github.com>
2023-08-12 16:16:54 +0200

    Update UiText.kt

--- UiText.kt
+++ UiText.kt
@@ -8,36 +8,8 @@ import androidx.annotation.StringRes
 
 sealed interface UiTextArg
 
-private interface PrimitiveUiTextArg<T : Any> : UiTextArg {
-	val value: T
-}
-
 @JvmInline
-private value class StringArg(val value: String) : UiTextArg
-
-@JvmInline
-private value class BooleanArg(override val value: Boolean) : PrimitiveUiTextArg<Boolean>
-
-@JvmInline
-private value class CharArg(override val value: Char) : PrimitiveUiTextArg<Char>
-
-@JvmInline
-private value class ByteArg(override val value: Byte) : PrimitiveUiTextArg<Byte>
-
-@JvmInline
-private value class ShortArg(override val value: Short) : PrimitiveUiTextArg<Short>
-
-@JvmInline
-private value class IntArg(override val value: Int) : PrimitiveUiTextArg<Int>
-
-@JvmInline
-private value class LongArg(override val value: Long) : PrimitiveUiTextArg<Long>
-
-@JvmInline
-private value class FloatArg(override val value: Float) : PrimitiveUiTextArg<Float>
-
-@JvmInline
-private value class DoubleArg(override val value: Double) : PrimitiveUiTextArg<Double>
+private value class ValueArg(val value: Any) : UiTextArg
 
 @JvmInline
 private value class BooleanResourceArg(@BoolRes val value: Int) : UiTextArg
@@ -48,51 +20,50 @@ private value class IntegerResourceArg(@IntegerRes val value: Int) : UiTextArg
 private data class StringResourceArg(
 	@StringRes
 	val value: Int,
-	val args: List<UiTextArg> = emptyList(),
+	val args: List<UiTextArg?> = emptyList(),
 ) : UiTextArg
 
 private data class PluralsResourceArg(
 	@PluralsRes
 	val value: Int,
 	val quantity: Int,
-	val args: List<UiTextArg> = emptyList(),
+	val args: List<UiTextArg?> = emptyList(),
 ) : UiTextArg
 
 
-private inline fun valueArg(any: Any?): UiTextArg = when (any) {
-	null         -> StringArg("")
-	is UiTextArg -> any
-	is String    -> StringArg(any)
-	is Boolean   -> BooleanArg(any)
-	is Char      -> CharArg(any)
-	is Byte      -> ByteArg(any)
-	is Short     -> ShortArg(any)
-	is Int       -> IntArg(any)
-	is Long      -> LongArg(any)
-	is Float     -> FloatArg(any)
-	is Double    -> DoubleArg(any)
-	else         -> StringArg(any.toString())
+private inline fun valueAsArg(value: Any?): UiTextArg? = when (value) {
+	null         -> null
+	is UiTextArg -> value
+	is String,
+	is Boolean,
+	is Char,
+	is Short,
+	is Int,
+	is Long,
+	is Float,
+	is Double    -> ValueArg(value)
+	else         -> ValueArg(value.toString())
 }
 
-private fun UiTextArg.getValue(context: Context): Any {
+private fun UiTextArg?.getValue(context: Context): Any? {
+	if (this == null) return null
+
 	return when (this) {
-		is StringArg             -> value
-		is PrimitiveUiTextArg<*> -> value
-		is BooleanResourceArg    -> context.resources.getBoolean(value)
-		is IntegerResourceArg    -> context.resources.getInteger(value)
-		is StringResourceArg     -> {
+		is ValueArg           -> value
+		is BooleanResourceArg -> context.resources.getBoolean(value)
+		is IntegerResourceArg -> context.resources.getInteger(value)
+		is StringResourceArg  -> {
 			context.getString(value).format(
 				*args.map { arg -> arg.getValue(context) }.toTypedArray()
 			)
 		}
-		is PluralsResourceArg    -> {
+		is PluralsResourceArg -> {
 			context.resources.getQuantityString(
 				value,
 				quantity,
 				*args.map { arg -> arg.getValue(context) }.toTypedArray(),
 			)
 		}
-		else                     -> error("The value $this is not supported as a valid UiTextArg.")
 	}
 }
 
@@ -103,17 +74,17 @@ fun integerResArg(@IntegerRes string: Int): UiTextArg = IntegerResourceArg(strin
 fun stringResArg(
 	@StringRes
 	string: Int,
-	args: List<UiTextArg> = emptyList(),
+	args: List<UiTextArg?> = emptyList(),
 ): UiTextArg = StringResourceArg(string, args)
 
 fun pluralsResArg(
 	@PluralsRes
 	string: Int,
 	quantity: Int,
-	args: List<UiTextArg> = emptyList(),
+	args: List<UiTextArg?> = emptyList(),
 ): UiTextArg = PluralsResourceArg(string, quantity, args)
 
-fun uiArgsOf(vararg args: Any?): List<UiTextArg> = args.map { arg -> valueArg(arg) }
+fun uiArgsOf(vararg args: Any?): List<UiTextArg?> = args.map { arg -> valueAsArg(arg) }
 
 
 sealed interface UiText
@@ -124,14 +95,14 @@ private value class DynamicString(val value: String) : UiText
 private data class StringResource(
 	@StringRes
 	val resId: Int,
-	val args: List<UiTextArg> = emptyList(),
+	val args: List<UiTextArg?> = emptyList(),
 ) : UiText
 
 private data class PluralsResource(
 	@PluralsRes
 	val resId: Int,
 	val quantity: Int,
-	val args: List<UiTextArg> = emptyList(),
+	val args: List<UiTextArg?> = emptyList(),
 ) : UiText
 
 
@@ -146,7 +117,7 @@ fun UiText(value: String?): UiText = when {
 
 fun UiText(
 	@StringRes resId: Int,
-	args: List<UiTextArg> = emptyList(),
+	args: List<UiTextArg?> = emptyList(),
 ): UiText = StringResource(
 	resId = resId,
 	args = args,
@@ -155,7 +126,7 @@ fun UiText(
 fun UiText(
 	@PluralsRes resId: Int,
 	quantity: Int,
-	args: List<UiTextArg> = emptyList(),
+	args: List<UiTextArg?> = emptyList(),
 ): UiText = PluralsResource(
 	resId = resId,
 	quantity = quantity,

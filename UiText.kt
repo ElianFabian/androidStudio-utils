adf9dfd0565bdb89c6d3920f9f7d5440b65e7968
Elián Fabián <86477169+ElianFabian@users.noreply.github.com>
2023-08-12 17:43:18 +0200

    Update UiText.kt

--- UiText.kt
+++ UiText.kt
@@ -1,10 +1,85 @@
 @file:Suppress("NOTHING_TO_INLINE")
 
+package com.elian.chatapp.core.util
+
 import android.content.Context
 import androidx.annotation.BoolRes
 import androidx.annotation.IntegerRes
 import androidx.annotation.PluralsRes
 import androidx.annotation.StringRes
+
+sealed interface UiText
+
+@JvmInline
+private value class DynamicString(val value: String) : UiText
+
+private data class StringResource(
+	@StringRes
+	val resId: Int,
+	val args: List<UiTextArg?> = emptyList(),
+) : UiText
+
+private data class PluralsResource(
+	@PluralsRes
+	val resId: Int,
+	val quantity: Int,
+	val args: List<UiTextArg?> = emptyList(),
+) : UiText
+
+
+private val EmptyUiText: UiText = DynamicString("")
+
+fun emptyUiText(): UiText = EmptyUiText
+
+fun UiText(value: String?): UiText = when {
+	value.isNullOrEmpty() -> emptyUiText()
+	else                  -> DynamicString(value)
+}
+
+fun UiText(
+	@StringRes resId: Int,
+	args: List<UiTextArg?> = emptyList(),
+): UiText = StringResource(
+	resId = resId,
+	args = args,
+)
+
+fun UiText(
+	@PluralsRes resId: Int,
+	quantity: Int,
+	args: List<UiTextArg?> = emptyList(),
+): UiText = PluralsResource(
+	resId = resId,
+	quantity = quantity,
+	args = args,
+)
+
+
+fun UiText?.asString(context: Context?): String {
+	if (context == null) return ""
+
+	return when (this) {
+		is DynamicString   -> value
+		is StringResource  -> {
+			val arguments = args.map { arg -> arg.getValue(context) }.toTypedArray()
+
+			context.getString(resId, *arguments)
+		}
+		is PluralsResource -> {
+			val arguments = args.map { arg -> arg.getValue(context) }.toTypedArray()
+
+			context.resources.getQuantityString(resId, quantity, *arguments)
+		}
+		else               -> ""
+	}
+}
+
+inline fun List<UiText>.concatAsString(context: Context?, separator: CharSequence = "\n"): String {
+	if (isEmpty() || context == null) return ""
+
+	return joinToString(separator) { it.asString(context) }
+}
+
 
 sealed interface UiTextArg
 
@@ -85,76 +160,3 @@ fun pluralsResArg(
 ): UiTextArg = PluralsResourceArg(string, quantity, args)
 
 fun uiArgsOf(vararg args: Any?): List<UiTextArg?> = args.map { arg -> valueAsArg(arg) }
-
-
-sealed interface UiText
-
-@JvmInline
-private value class DynamicString(val value: String) : UiText
-
-private data class StringResource(
-	@StringRes
-	val resId: Int,
-	val args: List<UiTextArg?> = emptyList(),
-) : UiText
-
-private data class PluralsResource(
-	@PluralsRes
-	val resId: Int,
-	val quantity: Int,
-	val args: List<UiTextArg?> = emptyList(),
-) : UiText
-
-
-private val EmptyUiText: UiText = DynamicString("")
-
-fun emptyUiText(): UiText = EmptyUiText
-
-fun UiText(value: String?): UiText = when {
-	value.isNullOrEmpty() -> emptyUiText()
-	else                  -> DynamicString(value)
-}
-
-fun UiText(
-	@StringRes resId: Int,
-	args: List<UiTextArg?> = emptyList(),
-): UiText = StringResource(
-	resId = resId,
-	args = args,
-)
-
-fun UiText(
-	@PluralsRes resId: Int,
-	quantity: Int,
-	args: List<UiTextArg?> = emptyList(),
-): UiText = PluralsResource(
-	resId = resId,
-	quantity = quantity,
-	args = args,
-)
-
-
-fun UiText?.asString(context: Context?): String {
-	if (context == null) return ""
-
-	return when (this) {
-		is DynamicString   -> value
-		is StringResource  -> {
-			val arguments = args.map { arg -> arg.getValue(context) }.toTypedArray()
-
-			context.getString(resId, *arguments)
-		}
-		is PluralsResource -> {
-			val arguments = args.map { arg -> arg.getValue(context) }.toTypedArray()
-
-			context.resources.getQuantityString(resId, quantity, *arguments)
-		}
-		else               -> ""
-	}
-}
-
-inline fun List<UiText>.concatAsString(context: Context?, separator: CharSequence = "\n"): String {
-	if (isEmpty() || context == null) return ""
-
-	return joinToString(separator) { it.asString(context) }
-}

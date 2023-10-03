152f34c16f6a039197c4c976905d2670b368efa4
Elián Fabián <86477169+ElianFabian@users.noreply.github.com>
2023-10-03 23:35:31 +0200

    Update ViewExt.kt

--- ViewExt.kt
+++ ViewExt.kt
@@ -4,11 +4,11 @@ private val ON_CLICK_LISTENERS_KEY = R.id.on_click_listeners
 
 private val View.onClickListeners: MutableList<OnClickListener>
 	get() {
-		val listeners = getTag(ON_CLICK_LISTENERS_KEY) as? MutableList<OnClickListener>
+		val listeners = getTag(ON_CLICK_LISTENERS_KEY) as? MutableList<OnClickListener> ?: mutableListOf()
 
 		setTag(ON_CLICK_LISTENERS_KEY, listeners)
 
-		return listeners ?: mutableListOf()
+		return listeners
 	}
 
 fun View.addOnClickListener(listener: OnClickListener) {
@@ -40,5 +40,22 @@ fun View.removeOnClickListener(listener: OnClickListener) {
 
 	if (listeners.isEmpty()) {
 		setOnClickListener(null)
+	}
+}
+
+suspend fun View.awaitClick() {
+	suspendCancellableCoroutine { continuation ->
+		val listener = object : OnClickListener {
+			override fun onClick(v: View) {
+				removeOnClickListener(this)
+				continuation.resume(Unit)
+			}
+		}
+
+		addOnClickListener(listener)
+
+		continuation.invokeOnCancellation {
+			removeOnClickListener(listener)
+		}
 	}
 }
